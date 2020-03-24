bb0bd183ba0c510793539cde108af9b7b2c7b4ff
opeyemidy <opeyemidy@gmail.com>
2020-03-24 21:53:03 +0100

    4th commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -12,7 +13,6 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import ProductZoomer from 'vue-product-zoomer'
-import store from './store'
 
 library.add(fas, fab)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
