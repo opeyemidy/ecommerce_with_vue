61bbd9c697b4d44ec269495c903949969ccd81d4
opeyemidy <opeyemidy@gmail.com>
2020-03-24 18:31:53 +0100

    add product page layout

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,21 +2,130 @@
   <div class="product">
     <b-container fluid class="container-color px-0">
       <b-row class="remove-row-margin custom-row-w mx-auto">
-        <b-col cols="9" class="pl-0">
-          <b-card class="remove-p-card-body">
-            <b-row class="remove-row-margin">
-              <b-col cols="8" class="p-0">
-                <ProductZoomer
-                  :base-images="images"
-                  :base-zoomer-options="zoomerOptions"
-                />
-              </b-col>
-              <b-col cols="4" class="p-0">dffggfgfg</b-col>
-            </b-row>
-          </b-card>
+        <b-col lg="9" md="12" class="pl-0">
+          <b-row class="mb-3">
+            <b-col cols="12">
+              <b-card class="remove-p-card-body">
+                <b-row class="remove-row-margin">
+                  <b-col lg="4" md="6" sm="12" class="p-0 mb-5">
+                    <ProductZoomer
+                      :base-images="images"
+                      :base-zoomer-options="zoomerOptions"
+                    />
+                  </b-col>
+                  <b-col lg="8" md="6" class="p-0 pl-5 mb-3">
+                    <h5>
+                      Shark Bottom Tide Shoes Men's Personality Sneakers -Black
+                    </h5>
+                    <hr />
+                    <div>₦50000</div>
+                  </b-col>
+                </b-row>
+              </b-card>
+            </b-col>
+          </b-row>
+          <b-row class="mb-3">
+            <b-col cols="12">
+              <b-card>
+                <h6>product details</h6>
+                <hr />
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Explicabo sequi dolor optio distinctio, cumque blanditiis
+                  corporis nam qui maxime dolore, facere, eligendi quisquam rem
+                  aperiam voluptatem vel facilis ab quidem? Sunt, provident!
+                  Blanditiis iure aliquid debitis voluptatum voluptas rem
+                  tempora deserunt soluta ea, harum voluptate dicta asperiores,
+                  distinctio minus cumque consequuntur. Maxime quo in minus
+                  neque magni est eaque tempore! Similique culpa inventore alias
+                  omnis vel. Enim, repellat. Quia, odit amet. Earum libero modi
+                  accusamus ab quibusdam expedita ratione animi excepturi
+                  molestiae harum, nihil a rerum delectus id quo est!
+                  Consequatur id ipsam vel similique inventore sint, perferendis
+                  quibusdam voluptas quos dicta rem iusto error maxime quia modi
+                  qui sed nihil tempora exercitationem tenetur ad perspiciatis!
+                  Deserunt dolore esse dolorum? Qui, repudiandae deleniti esse
+                  debitis porro voluptatem omnis rerum, a est laudantium vel ut
+                  sapiente quis, voluptas repellat expedita nisi? Numquam eos
+                  libero eligendi cum veniam optio odio repellendus eaque.
+                </p>
+              </b-card>
+            </b-col>
+          </b-row>
+          <b-row class="mb-3">
+            <b-col cols="12">
+              <b-card>
+                <h6>Specification</h6>
+                <hr />
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Explicabo sequi dolor optio distinctio, cumque blanditiis
+                  corporis nam qui maxime dolore, facere, eligendi quisquam rem
+                  aperiam voluptatem vel facilis ab quidem? Sunt, provident!
+                  Blanditiis iure aliquid debitis voluptatum voluptas rem
+                  tempora deserunt soluta ea, harum voluptate dicta asperiores,
+                  distinctio minus cumque consequuntur. Maxime quo in minus
+                  neque magni est eaque tempore! Similique culpa inventore alias
+                  omnis vel. Enim, repellat. Quia, odit amet. Earum libero modi
+                  accusamus ab quibusdam expedita ratione animi excepturi
+                  molestiae harum, nihil a rerum delectus id quo est!
+                  Consequatur id ipsam vel similique inventore sint, perferendis
+                  quibusdam voluptas quos dicta rem iusto error maxime quia modi
+                  qui sed nihil tempora exercitationem tenetur ad perspiciatis!
+                  Deserunt dolore esse dolorum? Qui, repudiandae deleniti esse
+                  debitis porro voluptatem omnis rerum, a est laudantium vel ut
+                  sapiente quis, voluptas repellat expedita nisi? Numquam eos
+                  libero eligendi cum veniam optio odio repellendus eaque.
+                </p>
+              </b-card>
+            </b-col>
+          </b-row>
+          <b-row class="mb-3">
+            <b-col cols="12">
+              <b-card>
+                <h6>Customers Feedback</h6>
+                <hr />
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Explicabo sequi dolor optio distinctio, cumque blanditiis
+                  corporis nam qui maxime dolore, facere, eligendi quisquam rem
+                  aperiam voluptatem vel facilis ab quidem? Sunt, provident!
+                  Blanditiis iure aliquid debitis voluptatum voluptas rem
+                  tempora deserunt soluta ea, harum voluptate dicta asperiores,
+                  distinctio minus cumque consequuntur. Maxime quo in minus
+                  neque magni est eaque tempore! Similique culpa inventore alias
+                  omnis vel. Enim, repellat. Quia, odit amet. Earum libero modi
+                  accusamus ab quibusdam expedita ratione animi excepturi
+                  molestiae harum, nihil a rerum delectus id quo est!
+                  Consequatur id ipsam vel similique inventore sint, perferendis
+                  quibusdam voluptas quos dicta rem iusto error maxime quia modi
+                  qui sed nihil tempora exercitationem tenetur ad perspiciatis!
+                  Deserunt dolore esse dolorum? Qui, repudiandae deleniti esse
+                  debitis porro voluptatem omnis rerum, a est laudantium vel ut
+                  sapiente quis, voluptas repellat expedita nisi? Numquam eos
+                  libero eligendi cum veniam optio odio repellendus eaque.
+                </p>
+              </b-card>
+            </b-col>
+          </b-row>
         </b-col>
-        <b-col cols="3" class="px-0">
-          <b-card class="remove-p-card-body">gvjvffhfhfhfvv</b-card>
+        <b-col cols="3" class="px-0 mb-5 d-none d-lg-block">
+          <b-card class="remove-p-card-body p-3"
+            >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+            cumque commodi eveniet vel dolorum quae rerum in ut atque aliquam
+            enim dolor officiis, rem, ullam et eius quisquam quasi harum. Id,
+            laudantium, vero atque quis laborum, modi accusantium quibusdam
+            cupiditate voluptates facere quod. Sint ipsa dolore beatae quia,
+            aperiam minima, velit porro possimus labore numquam totam quaerat
+            voluptatum modi assumenda! Cumque, earum reiciendis! Quis dolor
+            minima quam incidunt facilis, sequi veritatis nostrum est mollitia
+            quas odit. Omnis ut unde, impedit a aperiam iste laudantium! Aliquam
+            obcaecati deserunt voluptas officia asperiores? Nobis itaque quos
+            est voluptatem magni deleniti qui. Inventore ipsum iusto adipisci
+            dolor consequuntur tempora, dolorum quibusdam dicta, a animi alias
+            eius voluptas dolore odit, saepe cum consequatur dolorem
+            enim.</b-card
+          >
         </b-col>
       </b-row>
     </b-container>
